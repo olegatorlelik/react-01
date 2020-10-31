3e116aea922403fe19386cd012178424cb04dcb9
igorevich <lelik.belokonevvv@gmail.com>
2020-10-31 22:41:43 +0300

    ferst

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isSignegl: false,
+      text: "Hello",
+    };
+  }
+
+  sign = () => {
+    this.setState({ isSignegl: true });
+    console.log(this.state);
+  };
+
+  render() {
+    return (
+      <div>
+        {this.state.isSignegl ? (
+          <div>{this.state.text}</div>
+        ) : (
+          <div>
+            <button onClick={this.sign}>GO!!!</button>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
